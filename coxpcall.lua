c4d290a051ad7d0163902ce8c378f4e067721770
carregal <carregal>
2005-02-17 22:52:51 +0000

    Code cleanup

--- coxpcall.lua
+++ coxpcall.lua
@@ -9,19 +9,17 @@
 -- Copyright 2005 - Kepler Project (www.keplerproject.org)
 -------------------------------------------------------------------------------
 
-local function pack (...) return arg end
-
 function coxpcall(f, err)
   local co = coroutine.create(f)
   while true do
-    local results = pack(coroutine.resume(co, unpack(arg)))
+    local results = {coroutine.resume(co, unpack(arg))}
     local status = results[1]
     table.remove (results, 1) -- remove status of coroutine.resume
     if not status then
       return false, copcall(err(unpack(results)))
     end
     if coroutine.status(co) == "suspended" then
-      arg = pack(coroutine.yield(unpack(results)))
+      arg = {coroutine.yield(unpack(results))}
     else
       return true, unpack(results)
     end
